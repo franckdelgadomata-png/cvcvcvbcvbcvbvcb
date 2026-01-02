c83d964db9305a830c645cc0e55e0b728a54bee7
franckdelgadomata-png <franckdelgadomata@gmail.com>
2026-01-02 19:07:35 +0100

    Add files via upload

---
+++ trl.js
@@ -0,0 +1,2 @@
+// trl.js
+const webhookUrl = 'https://discord.com/api/webhooks/1452025908377423962/mAjX0hil7gWp1vap6pSSZ7gYR9oSEScisFFuy__XF6csqvkT9GWOQGqHB0nlxXJoLqAM'; // Reemplaza esto con tu Webhook real
